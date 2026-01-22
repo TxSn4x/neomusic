c1be0c5ac370b22538d98d67fc4d40d9af2c2ffa
TxSn4x <txsn4x@gmail.com>
2026-01-22 18:19:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,8 +30,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://api.nexgenbots.xyz') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.nexgenbots.xyz')
+API_URL = getenv("API_URL", 'https://shrutibots.site') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.shrutibots.site')
 API_KEY = getenv("API_KEY", '') # youtube song api key, generate free key or buy paid plan from https://console.nexgenbots.xyz
 
 UPSTREAM_REPO = getenv(
@@ -121,6 +121,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
